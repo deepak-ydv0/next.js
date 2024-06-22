0c0ad5c8c7895904baa2cb9057bc740eafdda755
DPK-YDV <kd806040@gmail.com>
2024-06-22 10:22:16 +0530

    music app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import FeaturedCourse from "@/components/FeaturedCourse";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instuctors from "@/components/Instuctors";
+import TestimonialCard from "@/components/TestimonialCard";
+import UpcommingWebinars from "@/components/UpcommingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Hello</h1>
+    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">
+        <HeroSection />
+        <FeaturedCourse />
+        <WhyChooseUs />
+        <TestimonialCard />
+        <UpcommingWebinars />
+        <Instuctors />
+        <Footer />
+      </h1>
+    </main>
   );
 }
